ec6c482c08bdc4ade50f2fe412f2055f2e3483c2
ThanhThankC <156212398+ThanhThankC@users.noreply.github.com>
2026-06-15 03:12:48 +0700

    feat: add WorldInputReceiver for sun collection

---
+++ Assets/Game/Scripts/Input/WorldInputReceiver.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class WorldInputReceiver : MonoBehaviour
+{
+    [SerializeField] private LayerMask sunLayer;
+
+    private readonly Collider2D[] sunHits = new Collider2D[5];
+
+    private Camera mainCamera;
+    private bool isGameStart;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        //TODO: Game State Manager Conventions
+        if (!isGameStart) return;
+
+        int count = Physics2D.OverlapPointNonAlloc(GetMouseWorldPos(), sunHits, sunLayer);
+        int loopCount = Mathf.Min(sunHits.Length, count);
+        for (int i = 0; i < loopCount; i++)
+            sunHits[i].GetComponent<Sun>()?.OnTap();
+    }
+
+    private Vector3 GetMouseWorldPos()
+    {
+        return mainCamera.ScreenToWorldPoint(Input.mousePosition);
+    }
+}
